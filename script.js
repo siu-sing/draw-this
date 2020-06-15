0158c3bca079c37248103fcae3ab21910a192e45
siu-sing <lai.siusing@gmail.com>
2020-06-15 22:02:13 +0800

    adding change to feature branch

--- script.js
+++ script.js
@@ -2,9 +2,36 @@ console.log("hello script js");
 
 // var px = "🍍";
 
-var px = "";
-outputElement = document.getElementById("output")
-outputElement.innerHTML = "What emoji would you like to use?"
+var px = "😽";
+// outputElement = document.getElementById("output")
+// outputElement.innerHTML = "What emoji would you like to use?"
+
+var setBoard = function(){
+  board = [
+    ["X","X","X","X"],
+    ["X","X","X","X"],
+    ["X","X","X","X"],
+    ["X","X","X","X"]
+  ]
+}
+
+var setCursorPos = function(i,j){
+  board[i][j]="c";
+}
+
+var displayBoard = function(){
+    board.forEach(row => {
+        var p = document.createElement("p")
+        p.innerHTML = row.join("")
+        document.getElementById("output").appendChild(p)
+    });
+}
+
+var board = [[]];
+var char = px;
+setBoard();
+setCursorPos(0,0);
+displayBoard();
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
@@ -13,45 +40,123 @@ var inputHappened = function(currentInput){
 
 var display="";
 
-
 var display = function(stuffToDisplay){
-  // your DOM manipulation code her
-
-    //user hasn't input an emoji
-    if (px===""){
-        //set emoji
-        px = stuffToDisplay;
-        outputElement.innerHTML = "";
-    } else {
-        //handling clear command
-        if(stuffToDisplay.toUpperCase().startsWith("CLEAR")==true){
-        clearP(stuffToDisplay.split(" ")[1]);
-        //handling multiple commands
-        } else {
-        //parse input
-        let inputArr = stuffToDisplay.split(" ");
-        //triangle
-        if (inputArr[0].toUpperCase() === "TRIANGLE") {
-            let triNum = inputArr[1];
-            let triArr = drawTriangle(triNum);
-            triArr.forEach(i => {
-                document.getElementById("output").appendChild(i);
-            });
-        //rtriangle
-        } else if (inputArr[0].toUpperCase() === "RTRIANGLE"){
-            let triNum = inputArr[1];
-            let triArr = drawRtriangle(triNum);
-            triArr.forEach(i => {
-                document.getElementById("output").appendChild(i);
-            });
-        } else {
-            inputArr.forEach(i=>{
-            document.getElementById("output").appendChild(drawRow(i));
-        });
-        }
-        }
-    }
 };
+
+//displayboard
+
+
+
+var setCharPos = function(i,j){
+  board[i][j]=char;
+}
+var getCursorPos = function(){
+  let i = "";
+  let j = "";
+    board.forEach((row,idx) => {
+      if(row.includes("c")==true){
+        i = idx; //set i
+        row.forEach((col,idx)=>{
+          if(col==="c"){
+            j = idx;
+          }
+        });
+      }
+    });
+  return [i,j]
+};
+var moveCursor = function(direction,distance){
+  let currPos = getCursorPos(); // [i,j]
+  let i = currPos[0];
+  let j = currPos[1];
+  setBoard();
+  switch (direction){
+    case "right":
+    setCursorPos(i,j+distance);
+    break;
+    case "left":
+    setCursorPos(i,j-distance);
+    break;
+    case "up":
+    setCursorPos(i-distance,j);
+    break;
+    case "down":
+    setCursorPos(i+distance,j);
+    break;
+  }
+}
+//draw function (direction, distance) - replaces coordinates with char
+//replace curr cursor position with char
+//move cursor in direction, replace
+var draw = function(direction,distance){
+
+  let currPos = getCursorPos(); // [i,j]
+  let i = currPos[0];
+  let j = currPos[1];
+
+  switch (direction){
+    case "right":
+      for(let c = 0; c < distance; c++){
+        setCharPos(i,j+c)
+      }
+    break;
+    case "left":
+      for(let c = 0; c < distance; c++){
+        setCharPos(i,j-c)
+      }
+    break;
+    case "up":
+      for(let c = 0; c < distance; c++){
+        setCharPos(i-c,j)
+      }
+    break;
+    case "down":
+      for(let c = 0; c < distance; c++){
+        setCharPos(i+c,j)
+        console.log(i,c)
+      }
+    break;
+  }
+}
+
+// var display = function(stuffToDisplay){
+//   // your DOM manipulation code her
+
+//     //user hasn't input an emoji
+//     if (px===""){
+//         //set emoji
+//         px = stuffToDisplay;
+//         outputElement.innerHTML = "";
+//     } else {
+//         //handling clear command
+//         if(stuffToDisplay.toUpperCase().startsWith("CLEAR")==true){
+//         clearP(stuffToDisplay.split(" ")[1]);
+//         //handling multiple commands
+//         } else {
+//         //parse input
+//         let inputArr = stuffToDisplay.split(" ");
+//         //triangle
+//         if (inputArr[0].toUpperCase() === "TRIANGLE") {
+//             let triNum = inputArr[1];
+//             let triArr = drawTriangle(triNum);
+//             triArr.forEach(i => {
+//                 document.getElementById("output").appendChild(i);
+//             });
+//         //rtriangle
+//         } else if (inputArr[0].toUpperCase() === "RTRIANGLE"){
+//             let triNum = inputArr[1];
+//             let triArr = drawRtriangle(triNum);
+//             triArr.forEach(i => {
+//                 document.getElementById("output").appendChild(i);
+//             });
+//         } else {
+//             inputArr.forEach(i=>{
+//             document.getElementById("output").appendChild(drawRow(i));
+//         });
+//         }
+//         }
+//     }
+// };
 
 var drawTriangle = function(num){
     var displayArr = [];
